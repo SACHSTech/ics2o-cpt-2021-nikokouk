11851adf98ca22dc299a73630ec12793ff502cda
NikoKouk <nicholas.koukoulidis23@ycdsbk12.ca>
2021-03-26 16:41:53 +0000

    1

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
-""" 
-A basic pygame template
-"""
- 
-import pygame
- 
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
-pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-  
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
+
+
+name = input("Please enter your name: ")
+print("welcome to Evading Malware The Game,", name, "lets get started!")
+print(" ")
+print("Its 1 o'clock in the afternoon,", name, " woke up and realized that they slept in. ")
+print("Now that you are up, what will you do?")
+print(" ")
+print("if you want to go on your computer, type 1")
+print("if you want to go downstairs, type 2")
+
+
+print(" ")
+
+answer1 = input("Which option do you want? ")
+
+
+if answer1 == "1":
+  print("yeah")
+elif answer1 == "2":
+    print("You then decide to go downstairs, and you think, what would I want to eat?")
+    print("enter")
+    food = input("what do you want to eat?: ")
+    print("enter")
+    print("You decide to make yourself some", food , "but somehow ended up burining it. How did you manage to mess that up?")
+    print("You then decide to go out on a walk but end up seeing one of your friends")
+    friend_name = input("What is your friends name?: ")
+    if friend_name == "i have no friends":
+      print("You remember that you have no friends and that reminds you, you should take your schizophrenia meds. You take your meds then proceed to keep walking")
+      print("You then proceed to walk 3 steps forward and get hit by a car, instantly killing you")
+      print(" ")
+      print("TRY AGAIN NEXT TIME!")
+    elif friend_name == "i dont have any friends":
+      print("You remember that you have no friends and that reminds you, you should take your schizophrenia meds. You take your meds then proceed to keep walking")
+      print("You then proceed to walk 3 steps forward and get hit by a car, instantly killing you")
+      print(" ")
+      print("TRY AGAIN NEXT TIME!")
+    else:
+      print("You say hi to", friend_name ,"and you continute to walk.")
+      print('You admire the scenery and say to yourself "this is some nice scenery" ')
+      print("At this point you have been walking for about 4 hours when you realize that you are hungry again")
+      print(" ")
+      print("You then see three resteraunts up ahead, Mcdonalds, KFC and Taco Bell ")
+      print(" ")
+      print("to go to mcdonalds, type 1")
+      print("to go to KFC, type 2")
+      print("to go to Taco Bell, type 3")
+      resteraunt = input("which resteraunt will you go to?: ")
+      if resteraunt == "1":
+        print("You ordered a big mac and enjoyed your meal but still feel hungry")
+      elif resteraunt == "2":
+        print("You ordered a bucket of chicken and enjoyed your meal, you are now full")
+      elif resteraunt == "3":
+        print("You ordered 20 soft tacos and that was too much for you to handle. You threw up half of it but still feel full")
+      print(" ")
+      print("As you are about to leave the resteraunt a car comes crashing through the side entrance")
